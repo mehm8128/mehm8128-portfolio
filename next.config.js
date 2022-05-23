ff0cc38ceed841c010adb4d9b5426c2036e0569b
mehm8128 <mehm8128@gmail.com>
2022-05-23 20:15:03 +0900

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const WindiCSSWebpackPlugin = require("windicss-webpack-plugin") //eslint-disable-line
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+	nextConfig,
+	webpack(config) {
+		config.plugins.push(new WindiCSSWebpackPlugin())
+		return config
+	},
+}
