8d7a23bf006f24443e3f3c40ce5959867734d543
mehm8128 <mehm8128@gmail.com>
2023-08-02 10:27:52 +0900

    twitterをxに

--- src/consts/links.ts
+++ src/consts/links.ts
@@ -1,5 +1,5 @@
 export const links = [
 	{ href: 'https://github.com/mehm8128', text: 'GitHub' },
 	{ href: 'https://trap.jp/author/mehm8128', text: 'traPブログ' },
-	{ href: 'https://twitter.com/mehm08128', text: 'Twitter' },
+	{ href: 'https://x.com/mehm08128', text: '𝕏' },
 ]
