4ed5ebf068cf83b92c70be5029d8e202d8652ba2
mehm8128 <mehm8128@gmail.com>
2024-02-04 16:51:45 +0900

    static exportするように

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const { withKumaUI } = require('@kuma-ui/next-plugin')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	output: 'export'
 }
 
 module.exports = withKumaUI(nextConfig)
