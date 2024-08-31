1ec5379e8da5f01a48db432b535ca959900dedf4
mehm8128 <mehm8128@gmail.com>
2024-08-31 19:04:29 +0900

    fix kuma config

--- kuma.config.ts
+++ kuma.config.ts
@@ -3,14 +3,14 @@ import { createTheme } from '@kuma-ui/core'
 const theme = createTheme({
 	colors: {
 		primary: '#7dd3fc',
-		secondary: '#af5718',
-	},
+		secondary: '#af5718'
+	}
 })
 
 type UserTheme = typeof theme
 
 declare module '@kuma-ui/core' {
-	export type Theme = UserTheme
+	export interface Theme extends UserTheme {}
 }
 
 export default theme
