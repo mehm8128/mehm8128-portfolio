60c8439fe2a51dbbb54a9e1f67b767e1e4f6427c
mehm8128 <mehm8128@gmail.com>
2024-09-22 20:46:36 +0900

    modify ui

--- src/app/_component/Blog.tsx
+++ src/app/_component/Blog.tsx
@@ -7,52 +7,52 @@ export default function Blog() {
 	return (
 		<SectionWrap headingText="ブログ記事">
 			<List>
-				<ListItem isNested>
+				<ListItem>
 					<StyledAnchor href="https://trap.jp/post/1775/">
 						Gitを作ってみようとした話
 					</StyledAnchor>
 				</ListItem>
-				<ListItem isNested>
+				<ListItem>
 					<StyledAnchor href="https://trap.jp/post/2036/">
 						Rustでmini-gitを作った話
 					</StyledAnchor>
 				</ListItem>
-				<ListItem isNested>
+				<ListItem>
 					<StyledAnchor href="https://trap.jp/post/1963/">
 						Kuma UIに†OSS Contribution†した話
 					</StyledAnchor>
 				</ListItem>
-				<ListItem isNested>
+				<ListItem>
 					<StyledAnchor href="https://trap.jp/post/2121/">
 						traPに入ってから最強Webエンジニアになるまで
 					</StyledAnchor>
 				</ListItem>
-				<ListItem isNested>
+				<ListItem>
 					<StyledAnchor href="https://trap.jp/post/2299/">
 						わたくしの開発環境を紹介させていただきます
 					</StyledAnchor>
 				</ListItem>
-				<ListItem isNested>
+				<ListItem>
 					<StyledAnchor href="https://trap.jp/post/2157/">
 						traP部員の凍結・除名について
 					</StyledAnchor>
 				</ListItem>
-				<ListItem isNested>
+				<ListItem>
 					<StyledAnchor href="https://trap.jp/post/1962/">
 						フロントエンドテストに入門してみた話
 					</StyledAnchor>
 				</ListItem>
-				<ListItem isNested>
+				<ListItem>
 					<StyledAnchor href="https://trap.jp/post/2153/">
 						VSCodeをカオスにしよう
 					</StyledAnchor>
 				</ListItem>
-				<ListItem isNested>
+				<ListItem>
 					<StyledAnchor href="https://trap.jp/post/1964/">
 						traQのa11yについて
 					</StyledAnchor>
 				</ListItem>
-				<ListItem isNested>
+				<ListItem>
 					<StyledAnchor href="https://trap.jp/post/2044/">
 						imgタグについて色んな仕様書とかドキュメントとか眺めてみた話
 					</StyledAnchor>
