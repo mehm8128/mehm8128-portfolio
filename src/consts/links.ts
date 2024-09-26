8d8b904a58cf2c1b86355223538b14eb0b28f52f
mehm8128 <mehm8128@gmail.com>
2024-09-26 23:36:44 +0900

    add zenn logo

--- src/consts/links.ts
+++ src/consts/links.ts
@@ -2,6 +2,7 @@ import GitHubLogo from '/public/github_logo.svg'
 import SizumeLogo from '/public/sizume_logo.svg'
 import traPLogo from '/public/traP_logo.svg'
 import XLogo from '/public/x_logo.svg'
+import zennLogo from '/public/zenn_logo.svg'
 
 export const links = [
 	{ src: GitHubLogo, href: 'https://github.com/mehm8128', alt: 'GitHub' },
@@ -9,6 +10,11 @@ export const links = [
 		src: traPLogo,
 		href: 'https://trap.jp/author/mehm8128',
 		alt: 'traPブログ'
+	},
+	{
+		src: zennLogo,
+		href: 'https://zenn.dev/mehm8128',
+		alt: 'Zenn'
 	},
 	{ src: XLogo, href: 'https://x.com/mehm08128', alt: '𝕏' },
 	{
