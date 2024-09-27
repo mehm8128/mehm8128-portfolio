07ed949e8f24d6f4e5ff702c6f9c39c9ecc6b35b
mehm8128 <mehm8128@gmail.com>
2024-09-27 12:48:15 +0900

    set ogp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,22 @@ import type { ReactNode } from 'react'
 import styles from './layout.module.css'
 
 export const metadata: Metadata = {
-	title: 'mehm8128-portfolio'
+	title: 'mehm8128-portfolio',
+	description: 'mehm8128のポートフォリオです。',
+	openGraph: {
+		type: 'website',
+		locale: 'ja_JP',
+		url: 'https://mehm8128-portfolio.vercel.app/',
+		title: 'mehm8128-portfolio',
+		description: 'mehm8128のポートフォリオです。',
+		siteName: 'mehm8128-portfolio'
+	},
+	twitter: {
+		card: 'summary',
+		site: '@mehm08128',
+		title: 'mehm8128-portfolio',
+		description: 'mehm8128のポートフォリオです。'
+	}
 }
 
 export default function RootLayout({
