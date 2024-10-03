2fcb417f48c62c6577f8f2bf25c8f4f852892471
mehm8128 <mehm8128@gmail.com>
2024-10-03 21:33:40 +0900

    remove seconds of last updated date

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,6 +1,6 @@
 export const formatDate = (date: Date) => {
 	const dateStr1 = date.toISOString().split('T')[0]
-	const dateStr2 = date.toISOString().split('T')[1].split('.')[0]
+	const dateStr2 = date.toISOString().split('T')[1].slice(0, 5)
 
 	return `${dateStr1} ${dateStr2}`
 }
