7be2f6c734e311f80a9a52373ec8cd4d6d33126b
yusuke-oyama <yusuke-oyama@cybozu.co.jp>
2024-11-16 20:40:46 +0900

    add bsky

--- src/consts/links.ts
+++ src/consts/links.ts
@@ -3,6 +3,7 @@ import SizumeLogo from '/public/sizume_logo.svg'
 import traPLogo from '/public/traP_logo.svg'
 import XLogo from '/public/x_logo.svg'
 import zennLogo from '/public/zenn_logo.svg'
+import bskyLogo from '/public/bsky_logo.svg'
 
 export const links = [
 	{ src: GitHubLogo, href: 'https://github.com/mehm8128', alt: 'GitHub' },
@@ -17,6 +18,11 @@ export const links = [
 		alt: 'Zenn'
 	},
 	{ src: XLogo, href: 'https://x.com/mehm8128', alt: '𝕏' },
+	{
+		src: bskyLogo,
+		href: 'https://bsky.app/profile/mehm8128.bsky.social',
+		alt: 'Bluesky'
+	},
 	{
 		src: SizumeLogo,
 		href: 'https://sizu.me/mehm8128',
