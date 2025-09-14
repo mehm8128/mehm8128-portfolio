6263c766e9de4adb3d78456676d35a3f1e7b2726
mehm8128 <mehm8128@gmail.com>
2025-09-14 22:35:32 +0900

    add link-card

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,22 @@ import { defineConfig } from "astro/config";
 import { rehypeHeadingIds } from "@astrojs/markdown-remark";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import remarkCallout from "@r4ai/remark-callout";
+import remarkLinkCard from "remark-link-card-plus";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://portfolio.hm8128.me",
   integrations: [mdx(), sitemap()],
   markdown: {
-    remarkPlugins: [remarkCallout],
+    remarkPlugins: [
+      remarkCallout,
+      [
+        remarkLinkCard,
+        {
+          cache: true,
+        },
+      ],
+    ],
     rehypePlugins: [
       // Astroの自動id付与はrehypeが走った後なので、先にidを付与してからautolinkを使う必要がある
       rehypeHeadingIds,
