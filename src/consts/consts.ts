c9d49c2a2ec3f86c5d3a0888cf98651d8b8eb032
mehm8128 <mehm8128@gmail.com>
2025-09-14 10:56:14 +0900

    adjust spacing

--- src/consts/consts.ts
+++ src/consts/consts.ts
@@ -3,4 +3,5 @@ import MEHM8128_ICON from "/public/mehm8128_circle.png";
 export const SITE_TITLE = "mehm8128";
 export const SITE_DESCRIPTION = "Hey👋";
 export const SITE_IMG = MEHM8128_ICON;
-export const ICON_DESCRIPTION = "mehm8128のアイコン画像";
+export const ICON_DESCRIPTION =
+  "mehm8128のアイコン。白背景に、青で大きく「mehm」という文字があり、中央部分に上から重ねて、小さく「めふも」と書かれている。さらにその文字の右隣には「</>」というHTMLタグのようなものがある。";
