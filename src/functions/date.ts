f00e076327aae194423c75400bd94f77db6eafa3
mehm8128 <mehm8128@gmail.com>
2025-09-14 16:20:59 +0900

    fix styles

--- src/functions/date.ts
+++ src/functions/date.ts
@@ -3,8 +3,6 @@ export const formatDate = (date: Date) => {
     year: "numeric",
     month: "2-digit",
     day: "2-digit",
-    hour: "2-digit",
-    minute: "2-digit",
     timeZone: "Asia/Tokyo",
   };
   return new Intl.DateTimeFormat("ja-JP", options).format(date);
