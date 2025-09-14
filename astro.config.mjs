c71c7e5f395adc697a9b63d50b0d66f3bd2fe67e
mehm8128 <mehm8128@gmail.com>
2025-09-14 21:09:38 +0900

    add autolinkheadings

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,34 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
+import { rehypeHeadingIds } from "@astrojs/markdown-remark";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://portfolio.hm8128.me",
   integrations: [mdx(), sitemap()],
+  markdown: {
+    rehypePlugins: [
+      // Astroの自動id付与はrehypeが走った後なので、先にidを付与してからautolinkを使う必要がある
+      rehypeHeadingIds,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "wrap",
+          content: {
+            type: "element",
+            tagName: "span",
+            properties: { class: "icon-link" },
+            children: [
+              {
+                type: "text",
+                value: " #",
+              },
+            ],
+          },
+        },
+      ],
+    ],
+  },
 });
