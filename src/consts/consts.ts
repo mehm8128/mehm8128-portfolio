5ad72ef43978450471b4e7e6ec0d74318d9c6069
mehm8128 <mehm8128@gmail.com>
2025-09-14 11:36:52 +0900

    fix ogp image

--- src/consts/consts.ts
+++ src/consts/consts.ts
@@ -1,7 +1,5 @@
-import MEHM8128_ICON from "/public/mehm8128_circle.png";
-
 export const SITE_TITLE = "mehm8128";
 export const SITE_DESCRIPTION = "Hey👋";
-export const SITE_IMG = MEHM8128_ICON;
+export const SITE_IMG = "/mehm8128_circle.png";
 export const ICON_DESCRIPTION =
   "mehm8128のアイコン。白背景に、青で大きく「mehm」という文字があり、中央部分に上から重ねて、小さく「めふも」と書かれている。さらにその文字の右隣には「</>」というHTMLタグのようなものがある。";
