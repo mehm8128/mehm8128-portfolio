9805b5be748f18847a9e7c466a1ef8c5bfb75f41
mehm8128 <mehm8128@gmail.com>
2025-09-14 16:22:39 +0900

    ブログ一覧をいい感じに (#247)
    * ブログ一覧をいい感じに
    
    * fix styles

--- src/content.config.ts
+++ src/content.config.ts
@@ -10,9 +10,8 @@ const blog = defineCollection({
       title: z.string(),
       description: z.string(),
       // Transform string to Date object
-      pubDate: z.coerce.date(),
+      publishedDate: z.coerce.date(),
       updatedDate: z.coerce.date().optional(),
-      heroImage: image().optional(),
     }),
 });
 
