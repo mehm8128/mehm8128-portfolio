b136674d6b2c073dd5c1876900477f430978f459
mehm8128 <mehm8128@gmail.com>
2025-09-14 15:17:47 +0900

    fix description

--- src/consts/consts.ts
+++ src/consts/consts.ts
@@ -1,5 +1,5 @@
 export const SITE_TITLE = "mehm8128";
-export const SITE_DESCRIPTION = "Hey👋";
+export const SITE_DESCRIPTION = "mehm8128のサイトです。";
 export const SITE_IMG = "/mehm8128_circle.png";
 export const ICON_DESCRIPTION =
   "mehm8128のアイコン。白背景に、青で大きく「mehm」という文字があり、中央部分に上から重ねて、小さく「めふも」と書かれている。さらにその文字の右隣には「</>」というHTMLタグのようなものがある。";
