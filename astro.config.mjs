e4139c15f493e9f3e414363bccaa8c39d7c998ca
mehm8128 <mehm8128@gmail.com>
2025-09-14 21:45:43 +0900

    add callout

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,14 @@ import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import { rehypeHeadingIds } from "@astrojs/markdown-remark";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import remarkCallout from "@r4ai/remark-callout";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://portfolio.hm8128.me",
   integrations: [mdx(), sitemap()],
   markdown: {
+    remarkPlugins: [remarkCallout],
     rehypePlugins: [
       // Astroの自動id付与はrehypeが走った後なので、先にidを付与してからautolinkを使う必要がある
       rehypeHeadingIds,
