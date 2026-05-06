c57eeca51d87918a8ee778a8401910c2d00cf681
mehm8128 <mehm8128@gmail.com>
2026-05-06 18:41:19 +0900

    add sponsoring section

---
+++ src/content/profile/sponsoring.ts
@@ -0,0 +1,18 @@
+interface Sponsoring {
+  text: string;
+  amount: string;
+  url: string;
+}
+
+export const sponsoringList: Sponsoring[] = [
+  {
+    text: "NV Access",
+    amount: "$5.00 AUD/Month",
+    url: "https://www.nvaccess.org/support-us/#donation-support",
+  },
+  {
+    text: "Accessibility Compat Data",
+    amount: "£5.00 GBP/Month",
+    url: "https://opencollective.com/lolas-lab/projects/acd",
+  },
+];
